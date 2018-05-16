d50dd2bb6974d1ebe990702071377541b1e046f8
byterub-project <39347162+byterub-project@users.noreply.github.com>
2018-05-16 21:58:20 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-extern const char* const MONERO_VERSION_TAG;
-extern const char* const MONERO_VERSION;
-extern const char* const MONERO_RELEASE_NAME;
-extern const char* const MONERO_VERSION_FULL;
+extern const char* const MONERO_VERSION_TAG; "@VERSIONTAG@"
+extern const char* const MONERO_VERSION; "0.11.2.0"
+extern const char* const MONERO_RELEASE_NAME; "ByteRub"
+extern const char* const MONERO_VERSION_FULL MONERO_VERSION "-" MONERO_VERSION_TAG
